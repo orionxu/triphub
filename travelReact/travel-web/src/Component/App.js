96cdc8fcc9d945150faa3be31df1c4eb4c6b0384
zyalm <zyalm1998@gmail.com>
2019-10-31 19:14:47 -0500

    set up react

---
+++ travelReact/travel-web/src/Component/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import TopNavBar from "./TopNavBar"
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavBar/>
+    </div>
+  );
+}
+
+export default App;
