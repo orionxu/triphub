bccacb8f4c3e906d3414899a20339c50dbd24cfc
zyalm <zyalm1998@gmail.com>
2019-11-12 15:33:50 -0600

    add map features

--- travelReact/travel-web/src/Component/Maps.js
+++ travelReact/travel-web/src/Component/Maps.js
@@ -1,26 +1,61 @@
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import React, {Component} from 'react';
+import CurrentLocation from './Map';
 
 class Maps extends Component {
+    state = {
+        showingInfoWindow: false,  //Hides or the shows the infoWindow
+        activeMarker: {},          //Shows the active marker upon click
+        selectedPlace: {}          //Shows the infoWindow to the selected place upon a marker
+    };
+
+    onMarkerClick = (props, marker, e) =>
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+
+    onClose = props => {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            });
+        }
+    };
 
     render() {
-        let mapStyles = {
-            width: '100%',
-            height: '100%',
-        };
         return (
-            <Map
+            <CurrentLocation
+                centerAroundCurrentLocation
                 google={this.props.google}
-                zoom={8}
-                style={mapStyles}
-                initialCenter={{ lat: 47.444, lng: -122.176}}
             >
-                <Marker position={{ lat: 48.00, lng: -122.00}} />
-            </Map>
+            {/*<Map*/}
+            {/*    google={this.props.google}*/}
+            {/*    zoom={8}*/}
+            {/*    style={mapStyles}*/}
+            {/*    initialCenter={{ lat: 47.444, lng: -122.176}}*/}
+            {/*>*/}
+                <Marker
+                    onClick={this.onMarkerClick}
+                    name={'current location'}
+                />
+                <InfoWindow
+                    marker={this.state.activeMarker}
+                    visible={this.state.showingInfoWindow}
+                    onclose={this.onClose}
+                >
+                    <div>
+                        <h4>{this.state.selectedPlace.name}</h4>
+                    </div>
+                </InfoWindow>
+            </CurrentLocation>
         );
     }
+
 }
 
 export default GoogleApiWrapper({
-    apiKey: 'AIzaSyB9UEy0mxFITcPW1UxMMFmfSx26U44uHLM'
+    apiKey: 'AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo'
 })(Maps);
