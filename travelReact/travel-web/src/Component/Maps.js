cad1eab3269032e52f7178ef9f145f8755434322
zyalm <zyalm1998@gmail.com>
2019-11-13 16:31:29 -0600

    background image

--- travelReact/travel-web/src/Component/Maps.js
+++ travelReact/travel-web/src/Component/Maps.js
@@ -1,8 +1,10 @@
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import React, {Component} from 'react';
 import CurrentLocation from './Map';
+import { Segment } from 'semantic-ui-react'
 
 class Maps extends Component {
+
     state = {
         showingInfoWindow: false,  //Hides or the shows the infoWindow
         activeMarker: {},          //Shows the active marker upon click
@@ -27,30 +29,30 @@ class Maps extends Component {
 
     render() {
         return (
-            <CurrentLocation
-                centerAroundCurrentLocation
-                google={this.props.google}
-            >
-            {/*<Map*/}
-            {/*    google={this.props.google}*/}
-            {/*    zoom={8}*/}
-            {/*    style={mapStyles}*/}
-            {/*    initialCenter={{ lat: 47.444, lng: -122.176}}*/}
-            {/*>*/}
-                <Marker
-                    onClick={this.onMarkerClick}
-                    name={'current location'}
-                />
-                <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                    onclose={this.onClose}
-                >
-                    <div>
-                        <h4>{this.state.selectedPlace.name}</h4>
-                    </div>
-                </InfoWindow>
-            </CurrentLocation>
+            <Segment.Group horizontal>
+                <Segment>left</Segment>
+                <Segment>middle</Segment>
+                <Segment>
+                    <CurrentLocation
+                        centerAroundCurrentLocation
+                        google={this.props.google}
+                    >
+                        <Marker
+                            onClick={this.onMarkerClick}
+                            name={'current location'}
+                        />
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}
+                            onclose={this.onClose}
+                        >
+                            <div>
+                                <h4>{this.state.selectedPlace.name}</h4>
+                            </div>
+                        </InfoWindow>
+                    </CurrentLocation>
+                </Segment>
+            </Segment.Group>
         );
     }
 
