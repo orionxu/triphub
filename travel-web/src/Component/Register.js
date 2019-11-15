72ff31a5025fe42a14b81cac468b07abf00eb465
orionxu <32559963+orionxu@users.noreply.github.com>
2019-11-15 01:44:03 -0600

    User Register

--- travel-web/src/Component/Register.js
+++ travel-web/src/Component/Register.js
@@ -3,6 +3,47 @@ import {Button, Form, Grid, Header, Image, Message, Segment} from 'semantic-ui-r
 import logo from '../Assets/images/logo.png'
 
 class RegisterForm extends Component {
+
+    constructor(props){
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleEmailChange = this.handleEmailChange.bind(this);
+        this.handlePWChange = this.handlePWChange.bind(this);
+        this.state = {
+            email: '',
+            password: '',
+        };
+    }
+
+    handleEmailChange(event){
+        this.setState({email: event.target.value});
+    }
+
+    handlePWChange(event){
+        this.setState({password: event.target.value});
+    }
+
+    handleSubmit(event){
+        event.preventDefault();
+        fetch('/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify(this.state)
+            }).then((response) => {
+            if (response.ok) {
+                let path = '/profile';
+                this.props.history.push(path)
+            } else {
+                alert("Sign up failed");
+                this.setState({email: ''});
+                this.setState({password: ''})
+            }
+        });
+    }
+
     render() {
         return (
             <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
@@ -10,15 +51,16 @@ class RegisterForm extends Component {
                     <Header as='h2' color='teal' textAlign='center'>
                         <img src={logo}/> Register your account
                     </Header>
-                    <Form size='large'>
+                    <Form size='large' onSubmit={this.handleSubmit}>
                         <Segment stacked>
-                            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address'/>
+                            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' onChange={this.handleEmailChange}/>
                             <Form.Input
                                 fluid
                                 icon='lock'
                                 iconPosition='left'
                                 placeholder='Password'
                                 type='password'
+                                onChange={this.handlePWChange}
                             />
                             <Form.Input
                                 fluid
@@ -28,7 +70,7 @@ class RegisterForm extends Component {
                                 type='password'
                             />
 
-                            <Button color='teal' fluid size='large'>
+                            <Button color='teal' fluid size='large' type='submit'>
                                 Sign Up
                             </Button>
                         </Segment>
--- triphub/accounts/api.py
+++ triphub/accounts/api.py
@@ -7,7 +7,11 @@ class UserRegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
 
     def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+        data_content = request.data
+        email = data_content['email']
+        del data_content['email']
+        data_content['username'] = email
+        serializer = self.get_serializer(data=data_content)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data})
@@ -21,7 +25,7 @@ class LoginAPI(generics.GenericAPIView):
         email = data_content['email']
         del data_content['email']
         data_content['username'] = email
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=data_content)
         if serializer.is_valid():
             return Response({'email': email})
         else:
