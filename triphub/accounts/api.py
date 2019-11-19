09c3e9fa3c409e442b578f1e9a437ecceba12bf0
orionxu <32559963+orionxu@users.noreply.github.com>
2019-11-18 18:10:57 -0600

    Token auth

--- triphub/accounts/api.py
+++ triphub/accounts/api.py
@@ -1,32 +1,28 @@
 from rest_framework import generics
 from .serializers import CreateUserSerializer, UserSerializer, LoginUserSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 
 class UserRegistrationAPI(generics.GenericAPIView):
     serializer_class = CreateUserSerializer
 
     def post(self, request, *args, **kwargs):
-        data_content = request.data
-        email = data_content['email']
-        del data_content['email']
-        data_content['username'] = email
-        serializer = self.get_serializer(data=data_content)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data})
+        token = Token.objects.get(user=user)
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,
+                         "token": token.key})
 
 
 class LoginAPI(generics.GenericAPIView):
     serializer_class = LoginUserSerializer
 
     def post(self, request, *args, **kwargs):
-        data_content = request.data
-        email = data_content['email']
-        del data_content['email']
-        data_content['username'] = email
-        serializer = self.get_serializer(data=data_content)
-        if serializer.is_valid():
-            return Response({'email': email})
-        else:
-            return Response({'email': email})
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data
+        token = Token.objects.get(user=user)
+        return Response({"user": UserSerializer(user, context=self.get_serializer_context()).data,
+                         "token": token.key})
--- triphub/settings.py
+++ triphub/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'triphub.accounts',
     'triphub.frontend',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
